bb01b2d4cf8fa1a0eaa7ee4f2d5e434c80bb4eeb
Lugres <lugres@gmail.com>
2024-03-04 10:23:53 -0500

    Update settings to ensure email psw reset on prod

--- django_project/settings.py
+++ django_project/settings.py
@@ -155,4 +155,13 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Email config
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# env.str("SECRET_KEY")
+EMAIL_BACKEND = env.str(
+    "EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
+)
+EMAIL_HOST = env.str("EMAIL_HOST", default="smtp.ukr.net")
+EMAIL_PORT = env.int("EMAIL_PORT", default=2525)
+EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=True)
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL")
